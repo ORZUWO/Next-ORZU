9a8b504cbb4ea598639447aa2406ceefa219f7ff
ORZU <orzu3008@gmail.com>
2026-05-12 17:22:18 +0500

    add

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { IStudent, IStudentCreate } from "./types"
 
-const apiBase = "http://localhost:8000"
+const apiBase = "http://localhost:3001"
 
 export const Todoapi = createApi({
   reducerPath: "Todoapi",
